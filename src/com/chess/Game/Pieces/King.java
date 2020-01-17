ffd5f460541da09a3442aed00877e419ea11f3c7
Carter Walsh <car4p17@gmail.com>
2020-01-17 01:09:47 -0500

    Add Basic King Moves

--- src/com/chess/Game/Pieces/King.java
+++ src/com/chess/Game/Pieces/King.java
@@ -4,11 +4,14 @@ import com.chess.Game.ChessBoard;
 import com.chess.Game.Coordinate;
 import com.chess.Game.Move;
 import com.chess.Game.NamedColor;
+import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece {
+    
+    protected int HEURISTIC_VALUE = 10;
 
     public King(NamedColor color, int player) {
         super(color, player);
@@ -22,7 +25,19 @@ public class King extends Piece {
     @Override
     public List<Move> getMoves(Coordinate position, ChessBoard boardState) {
         List<Move> moves = new ArrayList<>();
-        // TODO this
+        for (int x = position.getX() - 1; x <= position.getX() + 1; x++) {
+            for (int y = position.getY() - 1; y <= position.getY() + 1; y++) {
+                try {
+                    Coordinate to = new Coordinate(x, y);
+                    Piece p = boardState.getPiece(to);
+                    if (p == null || p.color != getColor()) {
+                        moves.add(new Move(position, to, p == null ? null : to));
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        }
         return moves;
     }
 
